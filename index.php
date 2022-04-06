a3e698665d6cf935bbe7d78d8288c190ab69db1a
lolfs <34860014+lolfs@users.noreply.github.com>
2022-04-06 21:16:57 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                 <a class="navbar-brand" href="">Timothys Webshop</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="Html/about.php">About</a></li>
                         <li class="nav-item dropdown">
@@ -33,13 +33,17 @@
                             </ul>
                         </li>
                     </ul>
-                    <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
-                            <i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                        </button>
-                    </form>
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+  <input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCMGFCcvrU3pu6ih61s95As4fbI+wNB4GqTkl7rwgBNbq0bgGHuUX2Gv5s0sP4LxE9QZXSgfQNvpdFOE67DO2bx56f+ZkH665fmiZ/+gsNVMYj56b7UVtlzqwJnuRRNjtunvi3+iCqa72XDbqUijyn/2Pz6/Nt+yKZuwN1WUzuWRjELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAgyrPQAyiNas4AwkTOsHzQ35UG+aT38DFH17os+L/IKjpFoRPOHXKxdl+eAGDqbcn/2JKa1NRl3UWDSoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjIwNDA2MTgyOTUyWjAjBgkqhkiG9w0BCQQxFgQUD3bhIqSLrtZlgdOo8zgUGFwOLlUwDQYJKoZIhvcNAQEBBQAEgYA+NzegTSv7mjeKs6H4KxhueFsUPU8o6TtCfAPaaJTgyyQdPwH3LhtQ2OIHf/ITGTGB8EwmBE2gnH2rz1X3Lg++B5qrrN6Y1HNokMWCkf+SjxtzO5ndSNhmRK4QV/HOsfen0BXFGNCyT9olc6R1nH5jCR8zTVPjqvRtL9nOZGa66Q==-----END PKCS7-----">
+<button type="submit" value="test" class="btn btn-outline-dark">
+	<i class="bi-cart-fill me-1"></i> View cart
+	</button>
+
+
+
+                        
+                </form>
                 </div>
             </div>
         </nav>
@@ -59,22 +63,22 @@
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="https://www.confectioneryproduction.com/wp-content/uploads/Japanese-sweet-bread-450x300.jpg" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                  <h5 class="fw-bolder">Place holder</h5>
-									10kr
+                                  <h5 class="fw-bolder">Bröd</h5>
+									30kr
+
 <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="VU7Q2NWUS9LK8">
+<input type="hidden" name="hosted_button_id" value="RZSUS866T3734">
 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent"> </div>
 <input type="submit" value="Add to cart" class="btn btn-outline-dark mt-auto" border="0" name="submit" alt="Add to cart">
 <img alt="" border="0" src="https://www.paypalobjects.com/sv_SE/i/scr/pixel.gif" width="1" height="1">
 </form>
 
-</form>
 
 
                                 </div>
@@ -85,19 +89,23 @@
                         <div class="card h-100">
                             
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="https://burgersandbangers.se/wp-content/uploads/2021/02/img_5490-1-450x300.jpg" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                    <h5 class="fw-bolder">Place holder&nbsp;</h5>
-                                    <!-- Product reviews-->
-                                  <!-- Product price-->
-                                <span class="text">$1</span> </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                    <h5 class="fw-bolder">Hamburgare</h5>
+								55kr
+
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="P5T35VVUMPAJS">
+<div class="card-footer p-4 pt-0 border-top-0 bg-transparent"> </div>
+<input type="submit" value="Add to cart" class="btn btn-outline-dark mt-auto" border="0" name="submit" alt="Add to cart">
+<img alt="" border="0" src="https://www.paypalobjects.com/sv_SE/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+								</div>
                             </div>
                         </div>
                     </div>
